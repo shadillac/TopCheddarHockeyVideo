7aafbf2c343161138e4a1337c81e6fa10da75e54
Shad Morris <shadmorris@gmail.com>
2014-09-08 21:52:14 -0700

    More error handling

--- TopCheddarHockey/phonesm-1.2.2/phonesm-1.2.2/Phone/HlsView.WP8/GameList.xaml.cs
+++ TopCheddarHockey/phonesm-1.2.2/phonesm-1.2.2/Phone/HlsView.WP8/GameList.xaml.cs
@@ -86,153 +86,162 @@ namespace HlsView
                         element = "vod-continuous";
                         break;
                 }
-                
-                foreach (XElement xe in xdoc.Descendants("game"))
+
+                if (xdoc.Descendants("game").Count() > 0)
                 {
-                    //Set Away Team Image
-                    try
-                    {
-                        imgAway[i] = new Image { Source = (ImageSource)new ImageSourceConverter().ConvertFromString(xe.Element("away-team").Element("logo-40px").Value) };
-                        imgAway[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                        imgAway[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                        imgAway[i].Margin = new Thickness(horizMargin + 0, heightMargin - 6, 0, 0);
-                        imgAway[i].Height = 45;
-                        imgAway[i].Width = 60;
-                        imgAway[i].Stretch = Stretch.Fill;
-                        ContentPanel.Children.Add(imgAway[i]);
-                    }
-                    catch
-                    {
-                        imgAway[i] = new Image();
-                        imgAway[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                        imgAway[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                        imgAway[i].Margin = new Thickness(horizMargin + 0, heightMargin - 6, 0, 0);
-                        imgAway[i].Height = 45;
-                        imgAway[i].Width = 60;
-                        imgAway[i].Stretch = Stretch.Fill;
-                        ContentPanel.Children.Add(imgAway[i]);
-                    }
 
-                    
-                    //Set Away Team Properties
-                    try
+                    foreach (XElement xe in xdoc.Descendants("game"))
                     {
-
-                        hlinkAway[i] = new Button { Content = xe.Element("away-team").Element("team-abbreviation").Value };
-                        hlinkAway[i].Tag = xe.Element("streams").Element("iphone").Element("away").Element(element).Value.ToString();
-                        //hlinkAway[i].TargetName = "_blank";
-                        hlinkAway[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                        hlinkAway[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                        hlinkAway[i].Margin = new Thickness(horizMargin + 60, heightMargin - 20, 0, 0);
-                        hlinkAway[i].Width = 100;
-                        hlinkAway[i].Click += GameList_Click;
-                        ContentPanel.Children.Add(hlinkAway[i]);
-                    }
-                    catch (Exception)
-                    {
-                        blkAway[i] = new TextBlock { Text = xe.Element("away-team").Element("team-abbreviation").Value };
-                        blkAway[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                        blkAway[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                        blkAway[i].Margin = new Thickness(horizMargin + 87, heightMargin, 0, 0);
-                        blkAway[i].FontSize = 22.667;
-                        ContentPanel.Children.Add(blkAway[i]);
-                    }
-
-                    try
-                    {
-                        scores = (string)userSettings["HideScores"];
-                    }
-                    catch (NullReferenceException)
-                    {
-                        scores = "0";
-                    }
-                    catch (KeyNotFoundException)
-                    {
-                        scores = "0";
-                    }
-
-                    if (scores != "1")
-                    {
+                        //Set Away Team Image
                         try
                         {
-                            //Set Away Goals
-                            awayGoals[i] = new TextBlock { Text = xe.Element("away-team").Element("goals").Value };
-                            awayGoals[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                            awayGoals[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                            awayGoals[i].Margin = new Thickness(horizMargin + 160, heightMargin, 0, 0);
-                            ContentPanel.Children.Add(awayGoals[i]);
+                            imgAway[i] = new Image { Source = (ImageSource)new ImageSourceConverter().ConvertFromString(xe.Element("away-team").Element("logo-40px").Value) };
+                            imgAway[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                            imgAway[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                            imgAway[i].Margin = new Thickness(horizMargin + 0, heightMargin - 6, 0, 0);
+                            imgAway[i].Height = 45;
+                            imgAway[i].Width = 60;
+                            imgAway[i].Stretch = Stretch.Fill;
+                            ContentPanel.Children.Add(imgAway[i]);
+                        }
+                        catch
+                        {
+                            imgAway[i] = new Image();
+                            imgAway[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                            imgAway[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                            imgAway[i].Margin = new Thickness(horizMargin + 0, heightMargin - 6, 0, 0);
+                            imgAway[i].Height = 45;
+                            imgAway[i].Width = 60;
+                            imgAway[i].Stretch = Stretch.Fill;
+                            ContentPanel.Children.Add(imgAway[i]);
+                        }
 
-                            ////Set @ sign
-                            //at[i] = new TextBlock { Text = "@" };
-                            //at[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                            //at[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                            //at[i].Margin = new Thickness(160, heightMargin, 0, 0);
-                            //ContentPanel.Children.Add(at[i]);
 
-                            //Set Home Goals
-                            homeGoals[i] = new TextBlock { Text = xe.Element("home-team").Element("goals").Value };
-                            homeGoals[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                            homeGoals[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                            homeGoals[i].Margin = new Thickness(horizMargin + 193, heightMargin, 0, 0);
-                            ContentPanel.Children.Add(homeGoals[i]);
+                        //Set Away Team Properties
+                        try
+                        {
+
+                            hlinkAway[i] = new Button { Content = xe.Element("away-team").Element("team-abbreviation").Value };
+                            hlinkAway[i].Tag = xe.Element("streams").Element("iphone").Element("away").Element(element).Value.ToString();
+                            //hlinkAway[i].TargetName = "_blank";
+                            hlinkAway[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                            hlinkAway[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                            hlinkAway[i].Margin = new Thickness(horizMargin + 60, heightMargin - 20, 0, 0);
+                            hlinkAway[i].Width = 100;
+                            hlinkAway[i].Click += GameList_Click;
+                            ContentPanel.Children.Add(hlinkAway[i]);
                         }
                         catch (Exception)
                         {
+                            blkAway[i] = new TextBlock { Text = xe.Element("away-team").Element("team-abbreviation").Value };
+                            blkAway[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                            blkAway[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                            blkAway[i].Margin = new Thickness(horizMargin + 87, heightMargin, 0, 0);
+                            blkAway[i].FontSize = 22.667;
+                            ContentPanel.Children.Add(blkAway[i]);
+                        }
+
+                        try
+                        {
+                            scores = (string)userSettings["HideScores"];
+                        }
+                        catch (NullReferenceException)
+                        {
+                            scores = "0";
+                        }
+                        catch (KeyNotFoundException)
+                        {
+                            scores = "0";
+                        }
+
+                        if (scores != "1")
+                        {
+                            try
+                            {
+                                //Set Away Goals
+                                awayGoals[i] = new TextBlock { Text = xe.Element("away-team").Element("goals").Value };
+                                awayGoals[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                                awayGoals[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                                awayGoals[i].Margin = new Thickness(horizMargin + 160, heightMargin, 0, 0);
+                                ContentPanel.Children.Add(awayGoals[i]);
+
+                                ////Set @ sign
+                                //at[i] = new TextBlock { Text = "@" };
+                                //at[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                                //at[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                                //at[i].Margin = new Thickness(160, heightMargin, 0, 0);
+                                //ContentPanel.Children.Add(at[i]);
+
+                                //Set Home Goals
+                                homeGoals[i] = new TextBlock { Text = xe.Element("home-team").Element("goals").Value };
+                                homeGoals[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                                homeGoals[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                                homeGoals[i].Margin = new Thickness(horizMargin + 193, heightMargin, 0, 0);
+                                ContentPanel.Children.Add(homeGoals[i]);
+                            }
+                            catch (Exception)
+                            {
+
+                            }
 
                         }
-                        
-                    }
 
-                    //Set Home Team Properties
-                    try
-                    {
-                        hlinkHome[i] = new Button { Content = xe.Element("home-team").Element("team-abbreviation").Value };
-                        hlinkHome[i].Tag = xe.Element("streams").Element("iphone").Element("home").Element(element).Value.ToString();
-                        //hlinkHome[i].TargetName = "_blank";
-                        hlinkHome[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                        hlinkHome[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                        hlinkHome[i].Margin = new Thickness(horizMargin + 205, heightMargin - 20, 0, 0);
-                        hlinkHome[i].Width = 100;
-                        hlinkHome[i].Click += GameList_Click;
-                        ContentPanel.Children.Add(hlinkHome[i]);
-                    }
-                    catch (Exception)
-                    {
-                        blkHome[i] = new TextBlock { Text = xe.Element("away-team").Element("team-abbreviation").Value };
-                        blkHome[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                        blkHome[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                        blkHome[i].Margin = new Thickness(horizMargin + 222, heightMargin, 0, 0);
-                        blkHome[i].FontSize = 22.667;
-                        ContentPanel.Children.Add(blkHome[i]);
-                    }
+                        //Set Home Team Properties
+                        try
+                        {
+                            hlinkHome[i] = new Button { Content = xe.Element("home-team").Element("team-abbreviation").Value };
+                            hlinkHome[i].Tag = xe.Element("streams").Element("iphone").Element("home").Element(element).Value.ToString();
+                            //hlinkHome[i].TargetName = "_blank";
+                            hlinkHome[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                            hlinkHome[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                            hlinkHome[i].Margin = new Thickness(horizMargin + 205, heightMargin - 20, 0, 0);
+                            hlinkHome[i].Width = 100;
+                            hlinkHome[i].Click += GameList_Click;
+                            ContentPanel.Children.Add(hlinkHome[i]);
+                        }
+                        catch (Exception)
+                        {
+                            blkHome[i] = new TextBlock { Text = xe.Element("away-team").Element("team-abbreviation").Value };
+                            blkHome[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                            blkHome[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                            blkHome[i].Margin = new Thickness(horizMargin + 222, heightMargin, 0, 0);
+                            blkHome[i].FontSize = 22.667;
+                            ContentPanel.Children.Add(blkHome[i]);
+                        }
 
-                    //Home Team Picture
-                    try
-                    {
-                        imgHome[i] = new Image { Source = (ImageSource)new ImageSourceConverter().ConvertFromString(xe.Element("home-team").Element("logo-40px").Value) };
-                        imgHome[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                        imgHome[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                        imgHome[i].Margin = new Thickness(horizMargin + 308, heightMargin - 6, 0, 0);
-                        imgHome[i].Height = 45;
-                        imgHome[i].Width = 60;
-                        imgHome[i].Stretch = Stretch.Fill;
-                        ContentPanel.Children.Add(imgHome[i]);
-                    }
-                    catch
-                    {
-                        imgHome[i] = new Image();
-                        imgHome[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                        imgHome[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                        imgHome[i].Margin = new Thickness(horizMargin + 308, heightMargin - 6, 0, 0);
-                        imgHome[i].Height = 45;
-                        imgHome[i].Width = 60;
-                        imgHome[i].Stretch = Stretch.Fill;
-                        ContentPanel.Children.Add(imgHome[i]);
-                    }
-                    
+                        //Home Team Picture
+                        try
+                        {
+                            imgHome[i] = new Image { Source = (ImageSource)new ImageSourceConverter().ConvertFromString(xe.Element("home-team").Element("logo-40px").Value) };
+                            imgHome[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                            imgHome[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                            imgHome[i].Margin = new Thickness(horizMargin + 308, heightMargin - 6, 0, 0);
+                            imgHome[i].Height = 45;
+                            imgHome[i].Width = 60;
+                            imgHome[i].Stretch = Stretch.Fill;
+                            ContentPanel.Children.Add(imgHome[i]);
+                        }
+                        catch
+                        {
+                            imgHome[i] = new Image();
+                            imgHome[i].VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                            imgHome[i].HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                            imgHome[i].Margin = new Thickness(horizMargin + 308, heightMargin - 6, 0, 0);
+                            imgHome[i].Height = 45;
+                            imgHome[i].Width = 60;
+                            imgHome[i].Stretch = Stretch.Fill;
+                            ContentPanel.Children.Add(imgHome[i]);
+                        }
 
-                    heightMargin = heightMargin + 55;
-                    i++;
+
+                        heightMargin = heightMargin + 55;
+                        i++;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No NHL games found on the chosen date.", "Videos not Found", MessageBoxButton.OK);
+                    NavigationService.GoBack();
                 }
             }
             catch (System.Reflection.TargetInvocationException)
