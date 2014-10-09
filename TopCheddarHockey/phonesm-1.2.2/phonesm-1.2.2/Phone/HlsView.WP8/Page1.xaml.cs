f5f7fbd505bb57879f016c37655d7783a62eaa2b
Shad Morris <shadmorris@gmail.com>
2014-10-09 13:44:55 -0700

    changed default date

--- TopCheddarHockey/phonesm-1.2.2/phonesm-1.2.2/Phone/HlsView.WP8/Page1.xaml.cs
+++ TopCheddarHockey/phonesm-1.2.2/phonesm-1.2.2/Phone/HlsView.WP8/Page1.xaml.cs
@@ -16,6 +16,7 @@ namespace HlsView
         {
             InitializeComponent();
             BuildLocalizedApplicationBar();
+            gameDate.Value = DateTime.Now.AddDays(-1);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
